b9bb40766d81a9e8ccda37b72408ac8ac1227fde
Carlos Pinto <carlos.pinto@udea.edu.co>
2023-05-08 08:58:41 -0500

    Método Login (Ingresar)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,12 @@ import { FullComponent } from './layouts/full/full.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   {
-    path: 'cafe',
+    path: 'joyeria',
     component: FullComponent,
     children: [
       {
         path: '',
-        redirectTo: '/cafe/dashboard',
+        redirectTo: '/joyeria/dashboard',
         pathMatch: 'full',
       },
       {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { SignupComponent } from '../signup/signup.component';
+import { LoginComponent } from '../login/login.component';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -17,6 +18,12 @@ export class HomeComponent implements OnInit {
     const dialogCongig = new MatDialogConfig();
     dialogCongig.width= "550px";
     this.dialog.open(SignupComponent, dialogCongig)
+  }
+
+  handleLoginAction(){
+    const dialogCongig = new MatDialogConfig();
+    dialogCongig.width= "550px";
+    this.dialog.open(LoginComponent, dialogCongig)
   }
 
 }

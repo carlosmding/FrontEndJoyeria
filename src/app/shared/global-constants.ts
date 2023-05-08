6d60b535dce69f211a1098cb6bb3825cc928300d
Carlos Pinto <carlos.pinto@udea.edu.co>
2023-05-08 08:43:49 -0500

    Método Signup completo (Registrarse)

---
+++ src/app/shared/global-constants.ts
@@ -0,0 +1,16 @@
+export class GlobalConstants{
+    //Message
+    public static genericError:string="No se pudo procesar, por favor intentelo de nuevo más tarde";
+
+    public static unauthroized:string ="Acceso no autorizado";
+    //Regex
+    public static nameRegex:string= "[a-zA-Z0-9 ]*";
+
+    public static emailRegex:string= "[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}";
+
+    public static contactNumberRegex:string= "^[e0-9]{10,10}$";
+
+    //Variable
+    public static error:string="error";
+
+}
